add24e60d9c683326365e6b08b1ec98836aeb380
Shane Bendak <shebah712@gmail.com>
2020-07-15 17:03:03 -0700

    [js/html] js practice

---
+++ js/pizza.js
@@ -0,0 +1,24 @@
+const menu = {
+    meatLover: {
+        toppings: ['Pepperoni', 'meatball', 'sausage'],
+        price: 12
+    },
+    gross: {
+        toppings: ['anchovie', 'pineapple', 'sourcream'],
+        price: 9
+    },
+    cheese: {
+        toppings: ['Cheese 1', 'Cheese 2', 'Cheese 3'],
+        price: 10
+    },
+    nuts: {
+        toppings: ['peanuts', 'walnuts', 'almonds'],
+        price: 9
+    },
+    supreme: {
+        toppings: ['green peppers', 'pepperoni', 'mushrooms'],
+        price: 10
+    }
+};
+ 
+console.log(Object.keys(menu));
